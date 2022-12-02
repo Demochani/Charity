8c678869ecf177593248dc66731972e3c22e79a3
nikita <dn200501@gmail.com>
2022-12-02 11:03:31 +0300

    all start folders added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="title-container">
+        <p className="title">War in Ukraine</p>
+        <img className="map" src="ua-map-color.png" alt="img" />
+      </div>
     </div>
   );
 }
---
+++ src/view/components/DonateCheck/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DonateCheck() {
+  return (
+    <div>index</div>
+  )
+}
