3f1a7b3f31943f2a981d14137a8f682ade6a8b05
nikita <dn200501@gmail.com>
2022-12-02 18:35:07 +0300

    donateForm is ready

--- src/view/components/DonateForm/index.tsx
+++ src/view/components/DonateForm/index.tsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import './donateForm.css'
-import LockIcon from '../../common/LockIcon'
-
+import React from "react";
+import "./donateForm.css";
+import LockIcon from "../../common/LockIcon";
 
 export default function DonateForm() {
   return (
-    <div className='donate-form-wrapper'>
+    <div className="donate-form-wrapper">
       <form>
-        <p className='donate-title'>Secure donation &#128274;</p>
-        <div className='button-dollar-container'>
+        <p className="donate-title">Secure donation &#128274;</p>
+        <div className="button-dollar-container">
           <button>$ 5</button>
           <button>$ 25</button>
           <button>$ 50</button>
+        </div>
+        <div className="button-dollar-container">
           <button>$ 200</button>
           <button>$ 500</button>
           <button>$ 1000</button>
         </div>
-        <input className='donate-input' type="text" placeholder='$'/>
-        <button className='donate-button'>Donate</button>
+        <input className="donate-input" type="text" placeholder="$" />
+        <button className="donate-button">Donate</button>
       </form>
     </div>
-  )
+  );
 }
